2ba09b9a20674c1cd7b46eabe00e305b41bfc54e
Abhijit Apte <abhijit.apte@gmail.com>
2016-05-14 23:03:49 +0530

    Initial Commit

---
+++ KDTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KDTree
+//
+//  Created by Abhijit Apte on 14/05/16.
+//  Copyright © 2016 Abhijit Apte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
